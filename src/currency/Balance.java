02367e9240c5321547d9912f333330d38a2ba161
ManiMukeshReddyNukal <114439598+ManiMukeshReddyNukal@users.noreply.github.com>
2022-12-06 12:40:24 -0500

    Base push

---
+++ src/currency/Balance.java
@@ -0,0 +1,56 @@
+package currency;
+
+
+import java.util.List;
+
+public class Balance{
+
+    private List<Currency> currencyList;
+    private float walletValueinUSD;
+
+
+    public List<Currency> getCurrencyList() {
+        return currencyList;
+    }
+
+    public void setCurrencyList(List<Currency> currencyList) {
+        this.currencyList = currencyList;
+    }
+
+    public float getWalletValueinUSD() {
+        return walletValueinUSD;
+    }
+
+    public void setWalletValueinUSD(float walletValueinUSD) {
+        this.walletValueinUSD = walletValueinUSD;
+    }
+
+    public void addbalance(Currency currency)
+    {
+        for (Currency cur : currencyList ) {
+            if (cur.getName().equals(currency.getName()))
+            {
+                cur.setQuantity(cur.getQuantity()+currency.getQuantity());
+            }
+            else {
+                currencyList.add(currency);
+            }
+        }
+
+    }
+
+    public void removebalance(Currency currency)
+    {
+        for (Currency cur : currencyList ) {
+            if (cur.getName().equals(currency.getName()) )
+            {
+                cur.setQuantity(cur.getQuantity()+currency.getQuantity());
+            }
+            else {
+                currencyList.add(currency);
+            }
+        }
+
+    }
+
+}
---
+++ src/currency/Currency.java
@@ -0,0 +1,42 @@
+package currency;
+
+public class Currency{
+   private String name;
+   private String symbol;
+   private float quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Currency{" +
+                "name='" + name + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
+
+
